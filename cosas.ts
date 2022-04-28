304d7243b9b8f0f83790c6f6c774703262a7b5f2
Franco Jalil <francojalil7@gmail.com>
2022-04-28 10:28:21 -0300

    firts commit

---
+++ cosas.ts
@@ -0,0 +1,13 @@
+const pelis = [{nombre: "Harry Potter", id: 5}, {nombre: "Encanto", id: 4}]
+
+function getAll(){
+    
+}
+function getById(id:number){
+    
+}
+
+export {
+    getById,
+    getAll
+}
